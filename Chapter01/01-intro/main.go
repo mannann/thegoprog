59369b5e34f0d5c13fe7bd98e6dec2f8672c3625
mannann <ninshar@pm.me>
2020-03-21 10:23:07 -0400

    Personal Info program

---
+++ Chapter01/01-intro/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Personal Information
+	fmt.Println("John Doe")
+	fmt.Println("ABC Street, Road No: 1")
+	fmt.Println("Andaman & Nicobar Islands")
+	fmt.Println("123-456-7890")
+	fmt.Println("M.Tech")
+}
